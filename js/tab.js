17d85c1956491468c362fa61435f9ef8a41f6269
Emili Garriga <porsilasmoscasweb@gmail.com>
2017-02-02 11:26:41 +0100

    tab Done

---
+++ js/tab.js
@@ -0,0 +1,14 @@
+
+    function chHeader(evt, navigator) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(navigator).style.display = "flex";
+        evt.currentTarget.className += " active";
+    }
